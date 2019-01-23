f9c35e28e5be4c1aefe2b841b3a0782da7b91d60
Sanyabelarus <00unclevasya00@mail.ru>
2019-01-23 06:49:22 +0300

    Initial commit

---
+++ src/com/company/AnnotationsInvocationHandler.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.lang.reflect.Method;
+import java.time.Instant;
+import java.time.Duration;
+
+public class AnnotationsInvocationHandler {
+  public static Object invoke(Object object, String method1, Object... args) throws Throwable {
+    Method method = methodFromMethodName(object, method1);
+
+    if (method.isAnnotationPresent(TimeForMethod.class)) {
+
+      Instant start = Instant.now();
+      Object returnObj = method.invoke(object, args);
+      Instant end = Instant.now();
+
+      System.out.println(
+          "Method " + method.getName() + " executed in " + Duration.between(start, end));
+
+      return returnObj;
+    }
+
+    return method.invoke(object, args);
+  }
+  // Возвращает метод по его имени
+  private static Method methodFromMethodName(Object object, String name) {
+    Method[] methods = object.getClass().getMethods();
+    int result = 0;
+    for (int i = 0; i < methods.length; i++) {
+      if (methods[i].getName().contains(name)) {
+        result = i;
+        break;
+      }
+    }
+    return methods[result];
+  }
+}
---
+++ src/com/company/NoAnnotation.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class NoAnnotation {
+  private String text = "Count counts ";
+
+  public String getText(int number) {
+    return text + number;
+  }
+}
